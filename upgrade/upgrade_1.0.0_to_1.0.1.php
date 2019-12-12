9afb4ea908b73310e53ff025f7b1e555bd7e2977
Jean-François Rivière <jean-francois.riviere@openedition.org>
2019-12-12 18:15:17 +0100

    editorial model v1.0.1

---
+++ upgrade/upgrade_1.0.0_to_1.0.1.php
@@ -0,0 +1,46 @@
+<?php
+if (PHP_SAPI != 'cli') die('cli only');         // php-cli only
+
+/*******************************************************************
+   
+   Description
+   - upgrade OpenEdition Books editorial model from version 1.0.0 to 1.0.1
+   - update XPath for pagination, ndlr, ndla
+     for compatibility with OpenEdition TEI Schema 1.6.2
+   - create if needed or update emversion option
+
+   Install
+   - copy or make a symbolic link of the file in the root directory 
+     of the Lodel install
+
+   Execute
+   - cd PATH_TO_ROOT_LODEL_DIRECTORY
+   - php upgrade_1.0.0_to_1.0.1.php mysite # update the site "mysite"
+     or 
+     php upgrade_1.0.0_to_1.0.1.php all # update all sites (excepted site listed in the array $exclude. See below)
+   - after execution, file should be remove from lodel root directory
+
+ *******************************************************************/
+
+require_once('lodel/install/scripts/me_manipulation_func.php');
+define('DO_NOT_DIE', true);                      // only die of a severe error
+// define('QUIET', true);                        // no output
+$exclude = array();                              // the $exclude array may contain site names to be excluded from processing at execution with the  parameter "all"
+$sites = new ME_sites_iterator($argv, 'errors'); // 'errors' display only errors ot the function ->m()
+
+while ($siteName = $sites->fetch()) {
+	if (in_array($siteName,$exclude)) continue;
+
+	TF::get('textes', 'pagination')->set('otx','/tei:TEI/teiHeader/fileDesc/sourceDesc/biblStruct/monogr/imprint/biblScope[@unit=\'page\']')->m();
+	TF::get('textes', 'ndlr')->set('otx','/tei:TEI/tei:text/tei:front/tei:note[@type=\'publisher\']/tei:p')->m();
+	TF::get('textes', 'ndla')->set('otx','/tei:TEI/tei:text/tei:front/tei:note[@type=\'author\']/tei:p')->m();
+
+	O::create('optionsgenerales', 'emversion', 'tinytext', 'Version du modèle éditorial',array('edition' => 'display','editionparams' => '','userrights' => 40,'defaultvalue' => '1.0.1','value'=>'1.0.1'))->m();
+	// Update emversion with MySQL commands
+	// me_manipulation_func.php doesn't provide method to update these fields
+	global $db;
+        $sql = lq("UPDATE options SET defaultvalue='1.0.1', value='1.0.1' WHERE name='emversion'");
+        $db->Execute($sql);
+}
+
+?>
